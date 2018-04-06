f03acba7d2340c39007e3b5a953a1b359a67de50
Gia <gia@coa.vn>
2018-04-07 00:07:00 +0700

    change processing order

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const auth = require('express-basic-auth')
 const port = process.env.PORT || 3000
 const app = express()
 
-app.use(compression())
-app.use(express.static('.'))
 app.use(auth({
   users: {
     'mis': 'kre',
     'gia': ''
   }
 }))
+app.use(compression())
+app.use(express.static('.'))
 
 app.listen(port)

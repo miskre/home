9fae4443341aad0434082894e40a217afca3d94e
Gia <gia@coa.vn>
2018-04-06 23:58:39 +0700

    add basic authen to express app

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const compression = require('compression')
+const auth = require('express-basic-auth')
 
 const port = process.env.PORT || 3000
 const app = express()
 
 app.use(compression())
 app.use(express.static('.'))
+app.use(basicAuth({
+  users: {
+    'mis': 'kre',
+    'gia': ''
+  }
+})
 
 app.listen(port)
